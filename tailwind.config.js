c72ab17a559bbc651d8f4af6782b256840dad9ea
Tenzin Sean <tenzin.jeunesse@gmail.com>
2022-03-09 21:19:12 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "netflix-black": "#000000",
+        "netflix-red": "#e50914",
+        "netflix-gray": "#303030",
+        "netflix-white": "#ffffff",  
+        "netflix-footer": "#757575"
+      }
+    },
+    fontFamily: {
+      Netflix: ["Netflix Sans", "Helvetica Neue", "Helvetica", "Arial", "sans-serif"]
+    }
+  },
+  plugins: [],
+}
