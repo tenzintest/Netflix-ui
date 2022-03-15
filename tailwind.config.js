b09670d225af47b4d94a6d938d475267275c5f36
Tenzin Sean <tenzin.jeunesse@gmail.com>
2022-03-15 21:22:23 +0100

    first page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   content: [],
   theme: {
+    fontSize: {
+      '2xl': ['24px', {
+        letterSpacing: '-0.01em',
+      }],
+      '3xl': ['32px', {
+        letterSpacing: '-0.02em',
+        lineHeight: '40px',
+      }],
+    },
     extend: {
       colors: {
         "netflix-black": "#000000",
@@ -8,6 +17,13 @@ module.exports = {
         "netflix-gray": "#303030",
         "netflix-white": "#ffffff",  
         "netflix-footer": "#757575"
+      },
+      width: {
+        '25': '9rem',
+        '128': '32rem',
+      },
+      maxHeight: {
+        '128': '72rem',
       }
     },
     fontFamily: {
